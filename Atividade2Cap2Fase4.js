00515958c426a8f714359ada69fb7b5fa29860e2
Marcus Gomes <111567346+marcusgoomes@users.noreply.github.com>
2022-11-30 09:44:03 -0300

    Add files via upload

---
+++ Atividade2Cap2Fase4.js
@@ -0,0 +1,107 @@
+polymer_bundled.min.js:1 Unrecognized feature: '
+'.
+_setUnmanagedPropertyToNode @ polymer_bundled.min.js:1
+characterData (assíncrono)
+run @ polymer_bundled.min.js:1
+setConfig @ polymer_bundled.min.js:1
+debounce @ polymer_bundled.min.js:1
+__debounceRender @ polymer_bundled.min.js:1
+re @ polymer_bundled.min.js:1
+se @ polymer_bundled.min.js:1
+ee @ polymer_bundled.min.js:1
+_propertiesChanged @ polymer_bundled.min.js:1
+_flushProperties @ polymer_bundled.min.js:1
+_flushProperties @ polymer_bundled.min.js:1
+ready @ polymer_bundled.min.js:1
+ready @ polymer_bundled.min.js:1
+_enableProperties @ polymer_bundled.min.js:1
+__enableOrFlushClients @ polymer_bundled.min.js:1
+_readyClients @ polymer_bundled.min.js:1
+_flushClients @ polymer_bundled.min.js:1
+_propertiesChanged @ polymer_bundled.min.js:1
+_flushProperties @ polymer_bundled.min.js:1
+_flushProperties @ polymer_bundled.min.js:1
+ready @ polymer_bundled.min.js:1
+_enableProperties @ polymer_bundled.min.js:1
+qe @ polymer_bundled.min.js:1
+(anônimo) @ polymer_bundled.min.js:1
+r @ polymer_bundled.min.js:1
+i @ polymer_bundled.min.js:1
+__ensureInstance @ polymer_bundled.min.js:1
+__render @ polymer_bundled.min.js:1
+(anônimo) @ polymer_bundled.min.js:1
+(anônimo) @ polymer_bundled.min.js:1
+(anônimo) @ polymer_bundled.min.js:1
+characterData (assíncrono)
+run @ polymer_bundled.min.js:1
+setConfig @ polymer_bundled.min.js:1
+debounce @ polymer_bundled.min.js:1
+__debounceRender @ polymer_bundled.min.js:1
+re @ polymer_bundled.min.js:1
+se @ polymer_bundled.min.js:1
+ee @ polymer_bundled.min.js:1
+_propertiesChanged @ polymer_bundled.min.js:1
+_flushProperties @ polymer_bundled.min.js:1
+_flushProperties @ polymer_bundled.min.js:1
+__enableOrFlushClients @ polymer_bundled.min.js:1
+_flushClients @ polymer_bundled.min.js:1
+_propertiesChanged @ polymer_bundled.min.js:1
+_flushProperties @ polymer_bundled.min.js:1
+_flushProperties @ polymer_bundled.min.js:1
+_invalidateProperties @ polymer_bundled.min.js:1
+_setProperty @ polymer_bundled.min.js:1
+Object.defineProperty.set @ polymer_bundled.min.js:1
+(anônimo) @ new_tab_page.js:219
+Promise.then (assíncrono)
+ready @ new_tab_page.js:219
+_enableProperties @ polymer_bundled.min.js:1
+connectedCallback @ polymer_bundled.min.js:1
+connectedCallback @ polymer_bundled.min.js:1
+connectedCallback @ new_tab_page.js:219
+(anônimo) @ new_tab_page.js:219
+textoativ = "html><head><title>Gulliver Traveller - Roteiros</title></head><body><b>->1 - Roteiros para *São Paulo*</b><br>A Terra da Garoa!<br>Fundada em 25 de janeiro de 1554 a cidade tem hoje cerca de 12 milhões de habitantes e é considerada o centro financeiro do Brasil e aqui vão 3 dicas de roteiros obrigatórios para aqueles que passam pela capital paulista<br>#Roteiro A | Região: Avenida Paulista<br>MASP; Parque Trianon; Rua Augusta<br>#Roteiro B | Região: Centro<br>Catedral da Sé; Pátio do Colégio; Rua Augusta<br>#Roteiro C | Região: Vila Madalena<br>Beco do Batman; Feirinha da Benedito Calixto; Livraria da Vila<br> <b>->2 - Roteiros para *Las Vegas*</b><br>Viva Las Vegas!<br>       A cidade mais populosa e mais densamente povoada do estado de Nevada, Las Vegas foi fundada em 1905 e é considerada uma cidade, oficialmente, desde 1911 e conta com mais de meio milhão de habitantes. Venha conhecer a capital dos jogos de azar!<br>#Roteiro A | Região: Las Vegas Boulevard South<br>Fonte do Bellagio; Principais Cassinos; Madame Tussauds<br>#Roteiro B | Região: Downtown<br>; Fremont; Las Vegas Art Museum; Museu nacional do Crime Organizado; <br>#Roteiro C | Região: Las Vegas Boulevard North<br>Outlet Premium North; Stratosphere; Apple Fashion Show<br><b>->3 - Roteiros para *Moscou*</b><br>Privet!<br>A capital Russa fica situada às margens do Rio Moscou e apesar de ser a cidade mais cosmopolita da Rússia, conta com grande resguardo de sua história soviética<br>#Roteiro A | Região: Praça Vermelha<br>Museu Histórico do Estado; Catedral de São Basílico; Mausoléu de Lênin<br>#Roteiro B | Região: Centro<br>Teatro Bolshoi; Monumento a Karl Marx; Rio Moscou<br>#Roteiro C | Região: Obras pela cidade<br>Metrô de Moscou; As Sete Irmãs; Moscow Leningradsky Railway Station  <br> </body> </html>"
+
+//Selecionar cidades
+
+var posicaocidade1 = textoativ.search("São Paulo");
+var posicaocidade2 = textoativ.search("Las Vegas");
+var posicaocidade3 = textoativ.search("Moscou");
+alert(posicaocidade1);
+alert(posicaocidade2);
+alert(posicaocidade3);
+undefined
+var roteiros =textoativ.split("#");
+console.log(roteiros);
+VM6924:2 (10) ['html><head><title>Gulliver Traveller - Roteiros</t…para aqueles que passam pela capital paulista<br>', 'Roteiro A | Região: Avenida Paulista<br>MASP; Parque Trianon; Rua Augusta<br>', 'Roteiro B | Região: Centro<br>Catedral da Sé; Pátio do Colégio; Rua Augusta<br>', 'Roteiro C | Região: Vila Madalena<br>Beco do Batma…. Venha conhecer a capital dos jogos de azar!<br>', 'Roteiro A | Região: Las Vegas Boulevard South<br>F…ellagio; Principais Cassinos; Madame Tussauds<br>', 'Roteiro B | Região: Downtown<br>; Fremont; Las Veg… Museum; Museu nacional do Crime Organizado; <br>', 'Roteiro C | Região: Las Vegas Boulevard North<br>O…om grande resguardo de sua história soviética<br>', 'Roteiro A | Região: Praça Vermelha<br>Museu Histór…; Catedral de São Basílico; Mausoléu de Lênin<br>', 'Roteiro B | Região: Centro<br>Teatro Bolshoi; Monumento a Karl Marx; Rio Moscou<br>', 'Roteiro C | Região: Obras pela cidade<br>Metrô de …eningradsky Railway Station  <br> </body> </html>']
+undefined
+// Localizando roteiros A
+alert(roteiros[1]);
+alert(roteiros[4]);
+alert(roteiros[7]);
+undefined
+var contlocais1 = roteiros[1].split(";");
+var contlocais2 = roteiros[4].split(";");
+var contlocais3 = roteiros[7].split(";");
+alert("O número de locais citados no Roteiro A da Região 1 é: "+contlocais1.length);
+alert("O número de locais citados no Roteiro A da Região 2 é: "+contlocais2.length);
+alert("O número de locais citados no Roteiro A da Região 3 é: "+contlocais3.length);
+undefined
+// Achando o número de caracteres totais
+var contador1 = roteiros[2].length;
+var contador2 = roteiros[5].length;
+console.log(contador1);
+console.log(contador2);
+VM7102:4 79
+VM7102:5 105
+undefined
+// Retornando os nomes dos locais no centro de São Paulo
+var centrosp = roteiros[2].substring(30,79);
+var centrosp = centrosp.split(";");
+alert(centrosp);
+
+// Retornando os nomes dos locais no bairro Downtown em Los Angeles
+var downtown = roteiros[5].substring(34,105);
+var downtown = downtown.split(";");
+alert(downtown);
+
+undefined
